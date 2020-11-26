a9854b217129b5a92c159bb7b34df29b265e5cc4
TunahanKorkmaz <tunahankorkmaz6@gmail.com>
2020-11-26 19:43:12 +0300

    webSite

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    new Typed('.firstText', {
+        strings: ["Hola, mi nombre es Tunahan"],
+        typeSpeed: 20,
+        startDelay: 900,
+        showCursor: false
+      });
+    }),
+    new Typed('.personalInfo', {
+        strings: ["I'm frontend software developer(Well at least trying to be!).<br> I am interested in web and mobile applications!👨‍💻"],
+        typeSpeed: 20,
+        startDelay: 1800,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoStart', {
+        strings: ["You can check out my"],
+        typeSpeed: 30,
+        startDelay: 5500,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoEnd', {
+        strings: ["By clicking the icon down below"],
+        typeSpeed: 20,
+        startDelay: 7000,
+        showCursor: false
+    }),
+    new Typed('.mailInfo', {
+        strings: ["Get in touch <a href='mailto:tunahankorkmaz6@gmail.com'>tunahankorkmaz6@gmail.com</a><span class='cursor'>_</span>"],
+        typeSpeed: 20,
+        startDelay: 8500,
+        showCursor: false
+    });
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "UA-138208404-1");
